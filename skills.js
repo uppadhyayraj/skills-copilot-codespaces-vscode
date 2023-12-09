42a91d7e7fa87bf5623fde9e2ab0d8094ecdc33f
uppadhyayraj <uppadhyayraj@gmail.com>
2023-12-09 22:47:49 +0000

    Add new funtion

--- skills.js
+++ skills.js
@@ -1,3 +1,9 @@
 function calculateNumber(var1, var2) {
   return Math.round(var1) + Math.round(var2);
 }
+function divideBy(var1, var2) {
+  if (var2 === 0) {
+    return 'Error';
+  }
+  return Math.round(var1) / Math.round(var2);
+}
